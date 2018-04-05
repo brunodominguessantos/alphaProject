11315a2b77b15577c9bae76c61149ceec7cd1af3
Bruno Santos <brunodominguessantos@gmail.com>
2018-04-05 20:52:25 +0100

    implemented ListView + List ControllerSkeleton

---
+++ src/main/java/org/academiadecodigo/alpha/controller/ListController.java
@@ -0,0 +1,117 @@
+package org.academiadecodigo.alpha.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ListController {
+
+    @FXML
+    private ImageView placeImage7;
+
+    @FXML
+    private ImageView placeImage6;
+
+    @FXML
+    private ImageView placeImage5;
+
+    @FXML
+    private ImageView placeImage4;
+
+    @FXML
+    private ImageView placeImage3;
+
+    @FXML
+    private ImageView placeImage2;
+
+    @FXML
+    private ImageView placeImage1;
+
+    @FXML
+    private ImageView GoodBtn1;
+
+    @FXML
+    private ImageView GoodBtn2;
+
+    @FXML
+    private ImageView GoodBtn3;
+
+    @FXML
+    private ImageView GoodBtn4;
+
+    @FXML
+    private ImageView GoodBtn5;
+
+    @FXML
+    private ImageView GoodBtn6;
+
+    @FXML
+    private ImageView GoodBtn7;
+
+    @FXML
+    private TextArea textArea1;
+
+    @FXML
+    private TextArea textArea2;
+
+    @FXML
+    private TextArea textArea3;
+
+    @FXML
+    private TextArea textArea4;
+
+    @FXML
+    private TextArea textArea5;
+
+    @FXML
+    private TextArea textArea6;
+
+    @FXML
+    private TextArea textArea7;
+
+    @FXML
+    private TextField City;
+
+    @FXML
+    void increasePoints(MouseEvent event) {
+
+    }
+
+    @FXML
+    void placeViewFive(MouseEvent event) {
+
+    }
+
+    @FXML
+    void placeViewFour(MouseEvent event) {
+
+    }
+
+    @FXML
+    void placeViewOne(MouseEvent event) {
+
+    }
+
+    @FXML
+    void placeViewSeven(MouseEvent event) {
+
+    }
+
+    @FXML
+    void placeViewSix(MouseEvent event) {
+
+    }
+
+    @FXML
+    void placeViewThree(MouseEvent event) {
+
+    }
+
+    @FXML
+    void placeViewTwo(MouseEvent event) {
+
+    }
+
+}
